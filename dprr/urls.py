fbacce25e7e5a9321052951336f5caa806dbd0c5
Luis Figueira <luis.figueira@kcl.ac.uk>
2014-02-06 17:22:19 +0000

    Fixed the urls.py file.

--- dprr/urls.py
+++ dprr/urls.py
@@ -1,12 +1,31 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.gis import admin
+from mezzanine.core.views import direct_to_template
+from mezzanine.conf import settings
 
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dprr.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       ('^admin/', include(admin.site.urls)),
+                       )
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+# Filebrowser admin media library.
+if getattr(settings, 'PACKAGE_NAME_FILEBROWSER') in settings.INSTALLED_APPS:
+    urlpatterns += patterns('',
+                            ('^admin/media-library/',
+                             include('%s.urls' %
+                                     settings.PACKAGE_NAME_FILEBROWSER)),
+                            )
+
+urlpatterns += patterns('',
+                        # homepage as static template
+                        url('^$', direct_to_template,
+                            {'template': 'index.html'}, name='home'),
+
+                        url('^', include('mezzanine.urls')),
+                        )
+
+# Adds ``STATIC_URL`` to the context of error pages, so that error
+# pages can use JS, CSS and images.
+handler404 = 'mezzanine.core.views.page_not_found'
+handler500 = 'mezzanine.core.views.server_error'

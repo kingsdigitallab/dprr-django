11e2fc6128418c829b4f3ac963ae271d5b4d783a
Luis Figueira <luis.figueira@kcl.ac.uk>
2014-04-10 15:45:12 +0100

    addend the Wagtail urls

--- dprr/urls.py
+++ dprr/urls.py
@@ -1,21 +1,30 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-
-import settings
 from django.contrib import admin
+
+from wagtail.wagtailcore import urls as wagtail_urls
+from wagtail.wagtailadmin import urls as wagtailadmin_urls
+from wagtail.wagtailimages import urls as wagtailimages_urls
+from wagtail.wagtailembeds import urls as wagtailembeds_urls
+from wagtail.wagtaildocs import admin_urls as wagtaildocs_admin_urls
+from wagtail.wagtaildocs import urls as wagtaildocs_urls
+from wagtail.wagtailsnippets import urls as wagtailsnippets_urls
+from wagtail.wagtailsearch.urls import frontend as \
+    wagtailsearch_frontend_urls, admin as wagtailsearch_admin_urls
+from wagtail.wagtailusers import urls as wagtailusers_urls
+from wagtail.wagtailredirects import urls as wagtailredirects_urls
+
+# Signal handlers
+from wagtail.wagtailsearch import register_signal_handlers as \
+    wagtailsearch_register_signal_handlers
+wagtailsearch_register_signal_handlers()
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'app.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
-
-
-#------------------------------------------------------------------------------
-# Django Debug Toolbar URLS
-#------------------------------------------------------------------------------
+    )
 
 try:
     if settings.DEBUG:
@@ -26,3 +35,32 @@ try:
 
 except ImportError:
     pass
+
+urlpatterns += patterns('',
+                        url(r'^wagtail/images/', include(wagtailimages_urls)),
+                        url(r'^wagtail/embeds/', include(wagtailembeds_urls)),
+                        url(r'^wagtail/documents/',
+                            include(wagtaildocs_admin_urls)),
+                        url(r'^wagtail/snippets/',
+                            include(wagtailsnippets_urls)),
+                        url(r'^wagtail/search/',
+                            include(wagtailsearch_admin_urls)),
+                        url(r'^wagtail/users/', include(wagtailusers_urls)),
+                        url(r'^wagtail/redirects/',
+                            include(wagtailredirects_urls)),
+                        url(r'^wagtail/', include(wagtailadmin_urls)),
+                        url(r'^search/', include(wagtailsearch_frontend_urls)),
+                        url(r'^documents/', include(wagtaildocs_urls)),
+
+                        url(r'', include(wagtail_urls)),
+                        )
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    import os.path
+
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL + 'images/',
+                          document_root=os.path.join(settings.MEDIA_ROOT,
+                                                     'images'))

1f19de484c9ff07913d7a96dce7be56ab7bf4658
Luis Figueira <luis.figueira@kcl.ac.uk>
2014-04-25 13:28:53 +0100

    Some improvement to admin display of praenomina, secondary sources and assertions.

--- promrep/admin.py
+++ promrep/admin.py
@@ -9,7 +9,6 @@ from models import Person, Office, Praenomen, \
 admin.site.register(AssertionType)
 admin.site.register(Date)
 admin.site.register(DateType)
-admin.site.register(SecondarySource)
 admin.site.register(RoleType)
 
 class AssertionPersonInline(admin.TabularInline):
@@ -40,9 +39,8 @@ admin.site.register(Office, OfficeAdmin)
 
 
 class AssertionAdmin(admin.ModelAdmin):
-
-    list_display = ('id', 'assertion_type', 'get_persons', 'secondary_source')
-    list_display_links = ('secondary_source', )
+    list_display = ('id', 'assertion_type', 'get_persons', 'display_text','secondary_source')
+    list_display_links = ('id', 'display_text', )
 
     readonly_fields=('id',)
 
@@ -54,9 +52,17 @@ admin.site.register(Assertion, AssertionAdmin)
 
 
 class PraenomenAdmin(admin.ModelAdmin):
-    list_display = ('abbrev', 'name')
+    list_display = ('id', 'abbrev', 'name')
     readonly_fields=('id',)
+    list_display_links = ('id', 'abbrev', 'name', )
 
 admin.site.register(Praenomen, PraenomenAdmin)
 
+class SecondarySourceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'abbrev_name', 'biblio')
+    readonly_fields=('id',)
+    list_display_links = ('id', 'abbrev_name', 'name', 'biblio')
+
+
+admin.site.register(SecondarySource, SecondarySourceAdmin)
 

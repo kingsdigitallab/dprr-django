b4046e113e3204f4587857c3f1b3f7354b511621
Luis Figueira <luis.figueira@kcl.ac.uk>
2014-06-05 15:07:20 +0100

    fixed the layout

--- promrep/models.py
+++ promrep/models.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.conf import settings
 from model_utils.models import TimeStampedModel
@@ -10,23 +12,31 @@ from django.core.urlresolvers import reverse
 
 
 class IntegerRangeField(models.IntegerField):
-    def __init__(self, verbose_name=None, name=None, min_value=None,
-            max_value=None, **kwargs):
-        self.min_value, self.max_value = min_value, max_value
+
+    def __init__(
+        self,
+        verbose_name=None,
+        name=None,
+        min_value=None,
+        max_value=None,
+        **kwargs
+        ):
+        (self.min_value, self.max_value) = (min_value, max_value)
         models.IntegerField.__init__(self, verbose_name, name, **kwargs)
 
     def formfield(self, **kwargs):
-        defaults = {'min_value': self.min_value, 'max_value': self.max_value}
+        defaults = {'min_value': self.min_value,
+                    'max_value': self.max_value}
         defaults.update(kwargs)
         return super(IntegerRangeField, self).formfield(**defaults)
 
 
 from south.modelsinspector import add_introspection_rules
-add_introspection_rules([],
-        ["^promrep\.models\.IntegerRangeField"])
+add_introspection_rules([], ["^promrep\.models\.IntegerRangeField"])
 
 
 class Praenomen(models.Model):
+
     abbrev = models.CharField(max_length=32, unique=True)
     name = models.CharField(max_length=128, unique=True)
 
@@ -34,12 +44,13 @@ class Praenomen(models.Model):
         return self.name
 
     class Meta:
+
         verbose_name_plural = 'Praenomina'
         ordering = ['name']
 
 
-
 class Sex(models.Model):
+
     name = models.CharField(max_length=64, unique=True)
 
     def __unicode__(self):
@@ -47,6 +58,7 @@ class Sex(models.Model):
 
 
 class RoleType(TimeStampedModel):
+
     name = models.CharField(max_length=128, unique=True)
 
     description = models.CharField(max_length=1024, blank=True)
@@ -56,106 +68,130 @@ class RoleType(TimeStampedModel):
 
 
 class Certainty(models.Model):
-    name = models.CharField( max_length=64, unique=True )
-    description = models.CharField( max_length=256, blank=True )
+
+    name = models.CharField(max_length=64, unique=True)
+    description = models.CharField(max_length=256, blank=True)
 
     def __unicode__(self):
         return self.name
 
 
 class Person(TimeStampedModel):
-    praenomen = models.ForeignKey( Praenomen )
-    nomen = models.CharField( max_length=128 )
 
-    cognomen_first = models.CharField( max_length=64 )
-    cognomen_other = models.CharField( max_length=128, blank=True )
+    original_text = models.CharField(max_length=256)
+
+    praenomen = models.ForeignKey(Praenomen)
+    nomen = models.CharField(max_length=128)
+
+    cognomen_first = models.CharField(max_length=64)
+    cognomen_other = models.CharField(max_length=128, blank=True)
 
     sex = models.ForeignKey(Sex)
 
-    tribe = models.CharField( max_length=128, blank=True )
+    tribe = models.CharField(max_length=128, blank=True)
 
-    is_patrician = models.BooleanField( default=False, blank=True, verbose_name = "Patrician" )
-    patrician_certainty = models.ForeignKey( Certainty, related_name='person_patrician_certainty' )
+    is_patrician = models.BooleanField(default=False, blank=True,
+            verbose_name='Patrician')
+    patrician_certainty = models.ForeignKey(Certainty,
+            related_name='person_patrician_certainty')
 
-    is_noble = models.BooleanField( default=False, blank=True, verbose_name = "Noble" )
-    noble_certainty = models.ForeignKey( Certainty, related_name='person_noble_certainty' )
+    is_noble = models.BooleanField(default=False, blank=True,
+                                   verbose_name='Noble')
+    noble_certainty = models.ForeignKey(Certainty,
+            related_name='person_noble_certainty')
 
-    is_novus_homo = models.BooleanField( default=False, blank=True, verbose_name = "Novus Homo" )
-    novus_homo_certainty = models.ForeignKey( Certainty, related_name='person_novus_homo_certainty' )
+    is_novus_homo = models.BooleanField(default=False, blank=True,
+            verbose_name='Novus Homo')
+    novus_homo_certainty = models.ForeignKey(Certainty,
+            related_name='person_novus_homo_certainty')
 
     notes = models.CharField(max_length=1024, blank=True)
     filliation = models.CharField(max_length=256, blank=True)
 
-    real_number = models.CharField(max_length=32, blank=True, verbose_name="RE Number")
-    real_number_old = models.CharField(max_length=32, blank=True, verbose_name="RE Number (Starred)")
+    real_number = models.CharField(max_length=32, blank=True,
+                                   verbose_name='RE Number')
+    real_number_old = models.CharField(max_length=32, blank=True,
+            verbose_name='RE Number (Starred)')
 
     real_attribute = models.CharField(max_length=128, blank=True)
-
 
     def cognomen(self):
         return self.cognomen_first + self.cognomen_other
 
     def real_id(self):
-        r_id = " ".join( [self.real_number, self.real_attribute] )
+        r_id = ' '.join([self.real_number, self.real_attribute])
 
         return r_id.strip()
 
     def get_name(self):
-        name_parts = [self.praenomen.abbrev, self.nomen, self.cognomen_first, self.cognomen_other]
+        name_parts = [self.praenomen.abbrev, self.nomen,
+                      self.cognomen_first, self.cognomen_other]
 
-        return " ".join(name_parts)
+        return ' '.join(name_parts)
 
     def url_to_edit_person(self):
-        url = reverse('admin:%s_%s_change' %(self._meta.app_label,  self._meta.module_name),  args=[self.id] )
-        return u'<a href="%s">%s</a>' %(url,  self.__unicode__())
+        url = reverse('admin:%s_%s_change' % (self._meta.app_label,
+                      self._meta.module_name), args=[self.id])
+        return u'<a href="%s">%s</a>' % (url, self.__unicode__())
 
     url_to_edit_person.allow_tags = True
     url_to_edit_person.short_description = 'Person'
 
     def __unicode__(self):
+
         # TODO: add praenomen, Re number
 
-        return self.get_name() + " (" + self.real_id() + ")"
+        return self.get_name() + ' (' + self.real_id() + ')'
 
 
-# broughton, etc
+# Broughton, Rupke, etc
+
 class SecondarySource(models.Model):
+
     name = models.CharField(max_length=256, unique=True)
 
-    abbrev_name = models.CharField(max_length=256, unique=True, blank=True)
+    abbrev_name = models.CharField(max_length=256, unique=True,
+                                   blank=True)
     biblio = models.CharField(max_length=512, unique=True, blank=True)
 
     def __unicode__(self):
         return self.abbrev_name
 
-#
-class PrimarySource(models.Model):
-    name = models.CharField(max_length=256, unique=True)
 
+#
+
+class PrimarySource(models.Model):
+
+    name = models.CharField(max_length=256, unique=True)
 
     def __unicode__(self):
         return self.name
 
 
 class Office(MPTTModel):
+
     name = models.CharField(max_length=256, unique=True)
     description = models.CharField(max_length=1024, blank=True)
 
-    parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+    parent = TreeForeignKey('self', null=True, blank=True,
+                            related_name='children')
 
     class Meta:
+
         verbose_name_plural = 'Offices'
         verbose_name = 'Office List'
         ordering = ['tree_id', 'lft', 'name']
 
     class MPTTMeta:
-        order_insertion_by = ['name',]
+
+        order_insertion_by = ['name']
 
     def __unicode__(self):
         return self.name
 
 
 class AssertionType(models.Model):
+
     name = models.CharField(max_length=128, unique=True)
 
     def __unicode__(self):
@@ -163,6 +199,7 @@ class AssertionType(models.Model):
 
 
 class Assertion(TimeStampedModel):
+
     persons = models.ManyToManyField(Person, through='AssertionPerson')
     assertion_type = models.ForeignKey(AssertionType)
     office = models.ForeignKey(Office)
@@ -175,7 +212,7 @@ class Assertion(TimeStampedModel):
     notes = models.CharField(max_length=1024, blank=True)
 
     def get_persons(self):
-        return "\n".join([str(a) for a in self.persons.all()])
+        return '\n'.join([str(a) for a in self.persons.all()])
 
 
 class AssertionPerson(TimeStampedModel):
@@ -188,48 +225,43 @@ class AssertionPerson(TimeStampedModel):
 
 
 class DateType(models.Model):
+
     name = models.CharField(max_length=32)
     created = models.DateTimeField(auto_now_add=True, editable=False)
     modified = models.DateTimeField(auto_now=True, auto_now_add=True,
-            editable=False)
+                                    editable=False)
 
     class Meta:
+
         ordering = ['name']
 
     def __unicode__(self):
-        return u'%s' % (self.name)
+        return u'%s' % self.name
 
 
 class Date(models.Model):
+
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey()
     date_type = models.ForeignKey(DateType, blank=True, null=True)
-    interval = models.SmallIntegerField(choices=settings.DATE_INTERVAL_CHOICES)
+    interval = \
+        models.SmallIntegerField(choices=settings.DATE_INTERVAL_CHOICES)
     year = IntegerRangeField(min_value=-500, max_value=500, blank=True,
                              null=True)
     year_uncertain = models.BooleanField(verbose_name='uncertain')
-    month = IntegerRangeField(min_value=1, max_value=12, blank=True, null=True)
+    month = IntegerRangeField(min_value=1, max_value=12, blank=True,
+                              null=True)
     month_uncertain = models.BooleanField(verbose_name='uncertain')
-    day = IntegerRangeField(min_value=1, max_value=31, blank=True, null=True)
+    day = IntegerRangeField(min_value=1, max_value=31, blank=True,
+                            null=True)
     day_uncertain = models.BooleanField(verbose_name='uncertain')
     circa = models.BooleanField()
     notes = models.TextField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, editable=False)
     modified = models.DateTimeField(auto_now=True, auto_now_add=True,
-            editable=False)
+                                    editable=False)
 
     def __unicode__(self):
-        return u'%s %s-%s-%s'.strip() % (self.date_type or '', self.year,
-                self.month, self.day)
-
-
-
-# TODO:
-#
-# multiple secondary sources -- multiple assertions ?????
-#
-# multiple secondary sources -- multiple primary sources (via primary source ref? -- original text)
-# no direct link between assertion and primary source ????
-
-
+        return u'%s %s-%s-%s'.strip() % (self.date_type or '',
+                self.year, self.month, self.day)
--- promrep/scripts/init_data.py
+++ promrep/scripts/init_data.py
@@ -1,18 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 # initial data for the promrep app
 
 from django.core.management import call_command
 
+
 def run():
-    print "Loading initial data fixtures"
+    print 'Loading initial data fixtures'
 
-    print "Loading Sex Data..."
-    call_command("loaddata", "promrep/fixtures/promrep_sex.json")
+    print 'Loading Sex Data...'
+    call_command('loaddata', 'promrep/fixtures/promrep_sex.json')
 
-    print "Loading Praenomen Data..."
-    call_command("loaddata", "promrep/fixtures/promrep_praenomina.json")
+    print 'Loading Praenomen Data...'
+    call_command('loaddata', 'promrep/fixtures/promrep_praenomina.json')
 
-    print "Loading Office Data..."
-    call_command("loaddata", "promrep/fixtures/promrep_offices.json")
+    print 'Loading Office Data...'
+    call_command('loaddata', 'promrep/fixtures/promrep_offices.json')
 
-    print "Loading Certainty Data..."
-    call_command("loaddata", "promrep/fixtures/promrep_certainty.json")
+    print 'Loading Certainty Data...'
+    call_command('loaddata', 'promrep/fixtures/promrep_certainty.json')

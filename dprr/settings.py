9e2b62fe96f50dbea4d09c90b320435901f85027
Luis Figueira <luis.figueira@kcl.ac.uk>
2014-06-10 15:09:27 +0100

    fixed some warnings related to the debug toolbar classes

--- dprr/settings.py
+++ dprr/settings.py
@@ -262,15 +262,14 @@ try:
     DEBUG_TOOLBAR_PATCH_SETTINGS = False
 
     DEBUG_TOOLBAR_PANELS = [
-        'debug_toolbar.panels.version.VersionDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.cache.CacheDebugPanel',
-        #'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.profiling.ProfilingDebugPanel',
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
     ]
 
 except ImportError:

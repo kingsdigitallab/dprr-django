4500e56103a3ffd52a751518742d2627214c91cb
Luis Figueira <luis.figueira@kcl.ac.uk>
2014-06-11 12:57:36 +0100

    added aux file for common functions; added new version of brennan data file.

--- promrep/scripts/add_brennan_data.py
+++ promrep/scripts/add_brennan_data.py
@@ -1,25 +1,71 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# add_brennan_data.py
-
 import csv
 
-# U flag: universal new-line mode
+from promrep.models import Assertion, AssertionPerson, AssertionType, \
+    Certainty, Date, DateType, Office, Person, Praenomen, RoleType, \
+    SecondarySource, Sex
 
+import promrep.scripts.data_import_aux
 
 def run():
 
-    ifile = open('promrep/scripts/data/BrennanExportv5.csv', 'rU')
+    # U flag: universal new-line mode
+
+    ifile = open('promrep/scripts/data/BrennanExportv6.csv', 'rU')
     reader = csv.reader(ifile, delimiter=',')
 
-    i = 0
     for row in reader:
-        print i
 
-        date, page, note_ref, assertion_type, praenomen, nomen, re_number, filiation, cognomen, is_patrician, is_patrician_certainty, consular_ancestor, consular_ancestor_certainty, calculated_father, suggested_father, suggested_father_certainty, calculated_grandfather, suggested_grandfather, suggested_grandfather_certainty, brother, brother_certainty, more_remote, more_remote_certainty, novus, novus_certainty = row
+        (
+            date,
+            page,
+            note_ref,
+            assertion_type,
+            praenomen,
+            nomen,
+            real_number,
+            filiation,
+            cognomen,
+            is_patrician,
+            is_patrician_certainty,
+            consular_ancestor,
+            consular_ancestor_certainty,
+            calculated_father,
+            suggested_father,
+            suggested_father_certainty,
+            calculated_grandfather,
+            suggested_grandfather,
+            suggested_grandfather_certainty,
+            brother,
+            brother_certainty,
+            more_remote,
+            more_remote_certainty,
+            novus,
+            novus_certainty,
+            province,
+            blank,
+            ) = row
 
-        i = i+1
+        # test if person exists
 
-        print nomen
 
+
+
+        data_import_aux.
+
+        identic_persons = Person.objects.filter(nomen=nomen.strip(),
+                real_number=real_number.strip())
+
+        if identic_persons.count() > 1:
+
+
+
+
+        print note_ref.strip(), blank.strip()
+
+
+        # print "testing ", nomen.strip(), real_number.strip(), identic_persons.count()
+
+        # print nomen

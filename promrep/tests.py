6a72965254135b3225fc01e464e992dffeba70d6
Luis Figueira <luis.figueira@kcl.ac.uk>
2014-09-03 18:15:18 +0100

    added a simple test for the Person model.

--- promrep/tests.py
+++ promrep/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from promrep.models import Praenomen, Tribe, Person
 
-# Create your tests here.
+class PersonTest(TestCase):
+
+    def create_person(self,
+        praenomen = Praenomen.objects.get(abbrev="M."),
+        nomen = "Antonius",
+        real_number="28",
+        cognomen=""):
+
+        return Person.objects.create(nomen=nomen, real_number=real_number, praenomen = praenomen)
+
+    def test_person_creation(self):
+        p = self.create_person()
+
+        self.assertTrue(isinstance(p, Person))
+        self.assertEqual(p.__unicode__(), p.praenomen.abbrev + ' ' + p.nomen + ' (' + p.real_number + ')')
+
+
+
+        # p2 = self.create_person(cognomen="Arius")
+

ca31108df5d6e0f5cc680d53ccc11cd38d2a36dc
Ginestra Ferraro <ginestra.ferraro@kcl.ac.uk>
2015-03-10 16:15:38 +0000

    views for UI and hgignore static
    --HG--
    branch : UI

--- dprr/views.py
+++ dprr/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
+# def index(request):
+#    return HttpResponse('DPRR Homepage... Watch this space!')
+
 def index(request):
-    return HttpResponse('DPRR Homepage... Watch this space!')
+	return render_to_response('homepage.html')

edb9269fe7a4bfe67aeaf511ba5ff1ac2912317f
Luis Figueira <luis.figueira@kcl.ac.uk>
2015-11-12 18:50:02 +0000

    more changes to the script: now writing a csv file for each "tree" in the database;
    --HG--
    branch : relationships

--- promrep/scripts/dump_relationship_data.py
+++ promrep/scripts/dump_relationship_data.py
@@ -6,23 +6,17 @@ from os import path
 from django.db.models import Q
 
 from promrep.models import Person, RelationshipAssertion, Praenomen, \
-    SecondarySource, PrimarySource, Sex, RelationshipType, \
-    RelationshipAssertionPrimarySource
-
-
-def print_relationship(rel):
-    print '{}; {}; {}; {}; {}'.format(rel.person.id, rel.person, rel.relationship, rel.related_person.id, rel.related_person, )
-
-
-def dump_all():
-    for p in Person.objects.all():
-        print_rels(p)
+    SecondarySource, PrimarySource, Sex, RelationshipType, RelationshipAssertionPrimarySource
 
 
 def dump_rels(person, relationships=[], visited=[]):
+    """Recursive function that given a person returns two lists
+    relationships - list of relationships linked to that person
+    visited - list of persons linked to that person via relationships
+    """
 
     if person in visited:
-        return relationships
+        return relationships, visited
 
     visited.append(person)
 
@@ -30,7 +24,7 @@ def dump_rels(person, relationships=[], visited=[]):
     i_rels = list(RelationshipAssertion.objects.filter(related_person=person))
 
     if not d_rels and not i_rels:
-        return relationships
+        return relationships, visited
 
     if d_rels:
         relationships += d_rels
@@ -43,20 +37,44 @@ def dump_rels(person, relationships=[], visited=[]):
     for rel in i_rels:
         dump_rels(rel.person, relationships, visited)
 
-    return relationships
+    return relationships, visited
 
 
 def run():
-    seed = 2597
+    visited_persons = set()
 
-    all_rels = set(dump_rels(Person.objects.get(id=seed)))
+    # for p in Person.objects.filter(id=4172):
+    for p in Person.objects.all():
+        print p.id
 
-#    persons = dump_person_rels(seed)
+        if p.id not in visited_persons:
+            visited_persons.add(p.id)
+            p_rels, visited = dump_rels(p, [], [])
 
-#    all_rels = RelationshipAssertion.objects.filter(
-#        Q(person__id__in=persons) | Q(related_person__id__in=persons))
+            for pers in visited:
+                visited_persons.add(pers.id)
 
-    print "rel.person.id; rel.person; rel.relationship; rel.related_person.id; rel.related_person"
-    for rel in all_rels:
-        # print rel
-        print_relationship(rel)
+            p_rels = set(p_rels)
+
+            if len(p_rels) > 0:
+
+                fname = "rel_csv/" + str(p.id) + ".csv"
+
+                with open(fname, 'wb') as csvfile:
+                    spamwriter = csv.writer(
+                        csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                    spamwriter.writerow(
+                        ["person1 ID", "person1", "relationship", "person2 ID", "person2"])
+
+                    for rel in p_rels:
+                        spamwriter.writerow(
+                            [rel.person.id, rel.person, rel.relationship, rel.related_person.id, rel.related_person])
+
+                csvfile.close()
+                print "Wrote {}".format(fname)
+
+            else:
+                print "No rels found for person {}".format(p.id)
+
+        else:
+            print "Skipping... {}".format(p.id)

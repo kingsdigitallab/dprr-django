f14d8f2a041741f7bc07db4752d9184c2ec11dcb
Ginestra Ferraro <ginestra.ferraro@kcl.ac.uk>
2016-02-10 18:36:50 +0000

    Search UI tests
    --HG--
    branch : UI

--- promrep/views.py
+++ promrep/views.py
@@ -88,3 +88,8 @@ class PromrepFacetedSearchView(FacetedSearchView):
 class PersonDetailView(DetailView):
     model = Person
     template_name = 'promrep/persons/detail.html'
+
+
+class PromrepFacetedSearchView2(PromrepFacetedSearchView):
+    template_name = 'search/search2.html'
+    

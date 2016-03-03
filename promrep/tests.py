980bdf559ff1b4998962fc6d55227d5264e1e452
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2016-03-03 12:13:49 +0000

    Added tests for the properties f and n of Person.
    --HG--
    branch : new-search-fields

--- promrep/tests.py
+++ promrep/tests.py
@@ -1,33 +1,41 @@
 from django.test import TestCase
-from promrep.models import Praenomen, Tribe, Person
+from promrep.models import Person
+
 
 class PersonTest(TestCase):
 
-    def create_person(self,
-        praenomen = Praenomen.objects.get(abbrev="M."),
-        nomen = "Antonius",
-        re_number="28",
-        cognomen=""):
+    def setUp(self):
+        self.filiations = [
+            {'text': None, 'f': None, 'n': None},
+            {'text': '', 'f': None, 'n': None},
+            {'text': 'ABC', 'f': None, 'n': None},
+            {'text': 'A. f.', 'f': 'A.', 'n': None},
+            {'text': 'A. n.', 'f': None, 'n': 'A.'},
+            {'text': 'A. or B. f.', 'f': 'A. or B.', 'n': None},
+            {'text': 'A. or B. n.', 'f': None, 'n': 'A. or B.'},
+            {'text': '- f.', 'f': None, 'n': None},
+            {'text': '- n.', 'f': None, 'n': None},
+            {'text': 'A. f. B. n.', 'f': 'A.', 'n': 'B.'},
+            {'text': 'A. or C. f. B. n.', 'f': 'A. or C.', 'n': 'B.'},
+            {'text': '- f. B. n.', 'f': None, 'n': 'B.'},
+            {'text': 'A. or C. f. B. or D. n.', 'f': 'A. or C.',
+             'n': 'B. or D.'},
+            {'text': 'A. or C. f. - n.', 'f': 'A. or C.', 'n': None},
+            {'text': '- f. - n.', 'f': None, 'n': None},
+        ]
+        self.person = Person()
 
-        return Person.objects.create(nomen=nomen, re_number=re_number, praenomen = praenomen)
+    def test__f(self):
+        for filiation in self.filiations:
+            self.person.filiation = filiation['text']
+            self.assertEqual(filiation['f'], self.person.f)
 
-    def test_person_creation(self):
-        p = self.create_person()
-
-        self.assertTrue(isinstance(p, Person))
-        self.assertEqual(p.__unicode__(), p.praenomen.abbrev + ' ' + p.nomen + ' (' + p.re_number + ')')
-
-    def test_person_update(self):
-        p1 = self.create_person()
-        p2 = Person(nomen=p1.nomen, re_number=p1.re_number, praenomen = p1.praenomen, cognomen="Arius")
-
-        p1.update_empty_fields(p2)
-
-        self.assertEqual(p1.cognomen, p2.cognomen)
-
-
-
-
-
-
-
+    def test__n(self):
+        # 1. fail the test
+        # self.fail()
+        # 2. easy pass of the test
+        # self.assertIsNone(self.person.n)
+        # 3. add test cases and iteratively implement the functionality to pass
+        for filiation in self.filiations:
+            self.person.filiation = filiation['text']
+            self.assertEqual(filiation['n'], self.person.n)

ba9da779e58a9998d5e78b5ca48ff5fef7b73766
lfigueira <luis.figueira@kcl.ac.uk>
2016-05-17 14:20:26 +0100

    added installed apps and haystack settings to test.py

--- dprr/settings/test.py
+++ dprr/settings/test.py
@@ -1,4 +1,4 @@
-from base import *
+from base import *  # noqa
 
 DEBUG = True
 TEMPLATE_DEBUG = True
@@ -15,10 +15,31 @@ CACHES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.db'
     },
 }
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.messages',
+    'django.contrib.sessions',
+    'django.contrib.staticfiles',
+    'author',
+
+    'dprr',
+    'promrep',
+)
 
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
 
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'promrep.solr_backends.'
+        'solr_backend_field_collapsing.GroupedSolrEngine',
+        'URL': 'http://127.0.0.1:8080/solr'
+    },
+}

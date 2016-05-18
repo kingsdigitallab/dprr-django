c0544de7f8c54906e3d8353907f5ba1a59a6cc80
lfigueira <luis.figueira@kcl.ac.uk>
2016-05-18 11:15:42 +0100

    removed installed apps from test settings

--- dprr/settings/test.py
+++ dprr/settings/test.py
@@ -19,27 +19,14 @@ DATABASES = {
     },
 }
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.messages',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'author',
-
-    'dprr',
-    'promrep',
-)
-
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
 
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'promrep.solr_backends.'
-        'solr_backend_field_collapsing.GroupedSolrEngine',
+        'ENGINE': ('promrep.solr_backends.'
+                   'solr_backend_field_collapsing.GroupedSolrEngine'),
         'URL': 'http://127.0.0.1:8080/solr'
     },
 }

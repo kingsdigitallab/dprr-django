74857ae866f031b0141e15429648a14eac30d3f9
lfigueira <luis.figueira@kcl.ac.uk>
2016-06-30 10:21:07 +0100

    added a simple test to PromrepFacetedSearchForm

--- promrep/tests.py
+++ promrep/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from promrep.models import Person
 
+from promrep.forms import PromrepFacetedSearchForm
+
 
 class PersonTest(TestCase):
 
@@ -39,3 +41,22 @@ class PersonTest(TestCase):
         for filiation in self.filiations:
             self.person.filiation = filiation['text']
             self.assertEqual(filiation['n'], self.person.n)
+
+
+class PromrepFacetedSearchFormTest(TestCase):
+
+    def setUp(self):
+        self.form = PromrepFacetedSearchForm()
+
+    def test_no_query_found(self):
+        self.assertIsNotNone(self.form.no_query_found())
+
+    def test_empty_search(self):
+        # cannot compare objects
+        # naively comparing first element and count of elements
+
+        expected = self.form.no_query_found()
+        result = self.form.search()
+
+        self.assertEqual(expected.count(), result.count())
+        self.assertEqual(expected[0].id, result[0].id)

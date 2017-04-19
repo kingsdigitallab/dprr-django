24ce493738b4728e047496b531d2450a63cac704
Brian Maher <brian.maher@kcl.ac.uk>
2017-04-19 15:35:07 +0000

    print fixed

--- assets/javascripts/config.js
+++ assets/javascripts/config.js
@@ -45,6 +45,20 @@ $(document).ready(function () {
         //Print the reloaded page
         window.print();
 
+        var new_url = document.URL;
+        if (new_url.includes("&printme=1"))
+        {
+            new_url = new_url.replace('&printme=1', '');
+        } else if (new_url.includes("?printme=1&"))
+        {
+            new_url = new_url.replace('?printme=1&', '?');
+        } else if (new_url.includes("?printme=1"))
+        {
+            new_url = new_url.replace('?printme=1', '');
+        }
+
+        window.location.href=new_url;
+
 
     }
 

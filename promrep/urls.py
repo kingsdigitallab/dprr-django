d0c067ee34a0fdc0550b2a44c728aae019b2cdee
Brian Maher <brian.maher@kcl.ac.uk>
2017-08-01 15:21:49 +0000

    Cache changes

--- promrep/urls.py
+++ promrep/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from promrep.views import (FastiSearchView, PersonDetailView,
                            PromrepFacetedSearchView, SenateSearchView,
                            get_relationships_network)
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     url(r'^$', PromrepFacetedSearchView.as_view(), name='haystack_search'),
@@ -10,5 +11,6 @@ urlpatterns = [
     url(r'^person/(?P<pk>\d+)/network/$', get_relationships_network,
         name='person-network'),
     url(r'^senate/$', SenateSearchView.as_view(), name='senate_search'),
-    url(r'^fasti/$', FastiSearchView.as_view(), name='fasti_search')
+    url(r'^fasti/$', cache_page(600 * 600)(FastiSearchView.as_view()),
+        name='fasti_search')
 ]

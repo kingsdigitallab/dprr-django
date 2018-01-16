01ba30fa75fdda3e01ceadae31266657ca7dceeb
Ginestra Ferraro <ginestra@users.noreply.github.com>
2018-01-16 15:43:03 +0000

    Fixed save pdf display

--- dprr/settings/base.py
+++ dprr/settings/base.py
@@ -309,15 +309,7 @@ PDFKIT_OPTIONS = {
     'page-size': 'A4',
     'margin-top': '0.75in',
     'margin-right': '0.75in',
-    'margin-bottom': '0.5in',
+    'margin-bottom': '0.75in',
     'margin-left': '0.75in',
-    'encoding': "UTF-8",
-    'custom-header' : [
-        ('Accept-Encoding', 'gzip')
-    ],
-    'cookie': [
-        ('cookie-name1', 'cookie-value1'),
-        ('cookie-name2', 'cookie-value2'),
-    ],
-    'no-outline': None
+    'encoding': "UTF-8"
 }

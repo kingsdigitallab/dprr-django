119429c9c8fc76fce357559a1bc0659bac308c8f
Elliott Hall <elliott.hall@kcl.ac.uk>
2021-09-24 12:41:39 +0100

    wagtail name fix

--- wagtailbase/base.py
+++ wagtailbase/base.py
@@ -5,14 +5,17 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db import models
 from django.shortcuts import render
 from django.template.loader import select_template
-from wagtail.contrib.wagtailroutablepage.models import RoutablePageMixin, route
-from wagtail.wagtailadmin.edit_handlers import (FieldPanel, MultiFieldPanel,
-                                                PageChooserPanel)
-from wagtail.wagtailcore.fields import RichTextField
-from wagtail.wagtailcore.models import Page
-from wagtail.wagtaildocs.edit_handlers import DocumentChooserPanel
-from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
-from wagtail.wagtailsearch import index
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    PageChooserPanel,
+)
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+from wagtail.documents.edit_handlers import DocumentChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
 logger = logging.getLogger(__name__)
 
@@ -20,16 +23,19 @@ logger = logging.getLogger(__name__)
 class AbstractLinkField(models.Model):
 
     """Abstract class for link fields."""
-    link_document = models.ForeignKey('wagtaildocs.Document', blank=True,
-                                      null=True, related_name='+')
-    link_external = models.URLField('External link', blank=True, null=True)
-    link_page = models.ForeignKey('wagtailcore.Page', blank=True,
-                                  null=True, related_name='+')
+
+    link_document = models.ForeignKey(
+        "wagtaildocs.Document", blank=True, null=True, related_name="+"
+    )
+    link_external = models.URLField("External link", blank=True, null=True)
+    link_page = models.ForeignKey(
+        "wagtailcore.Page", blank=True, null=True, related_name="+"
+    )
 
     panels = [
-        DocumentChooserPanel('link_document'),
-        FieldPanel('link_external'),
-        PageChooserPanel('link_page')
+        DocumentChooserPanel("link_document"),
+        FieldPanel("link_external"),
+        PageChooserPanel("link_page"),
     ]
 
     @property
@@ -48,12 +54,10 @@ class AbstractLinkField(models.Model):
 class AbstractRelatedLink(AbstractLinkField):
 
     """Abstract class for related links."""
-    title = models.CharField(max_length=256, help_text='Link title')
 
-    panels = [
-        FieldPanel('title'),
-        MultiFieldPanel(AbstractLinkField.panels, 'Link')
-    ]
+    title = models.CharField(max_length=256, help_text="Link title")
+
+    panels = [FieldPanel("title"), MultiFieldPanel(AbstractLinkField.panels, "Link")]
 
     class Meta:
         abstract = True
@@ -61,19 +65,19 @@ class AbstractRelatedLink(AbstractLinkField):
 
 class AbstractAttachment(AbstractLinkField):
     image = models.ForeignKey(
-        'wagtailimages.Image',
+        "wagtailimages.Image",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name='+'
+        related_name="+",
     )
     embed_url = models.URLField("Embed URL", blank=True)
     caption = models.CharField(max_length=255, blank=True)
 
     panels = [
-        ImageChooserPanel('image'),
-        FieldPanel('embed_url'),
-        FieldPanel('caption'),
+        ImageChooserPanel("image"),
+        FieldPanel("embed_url"),
+        FieldPanel("caption"),
         MultiFieldPanel(AbstractLinkField.panels, "Link"),
     ]
 
@@ -110,8 +114,11 @@ class BasePage(Page):
 
         parent = self.get_parent().specific
 
-        if parent and isinstance(
-                parent, BasePage) and parent.is_current_or_ancestor(page):
+        if (
+            parent
+            and isinstance(parent, BasePage)
+            and parent.is_current_or_ancestor(page)
+        ):
             return True
 
         return False
@@ -119,16 +126,14 @@ class BasePage(Page):
     def get_template(self, request, *args, **kwargs):
         """Checks if there is a template with the page path, and uses that
         instead of using the generic page type template."""
-        page_template = '{0}.html'.format(self.url.strip('/'))
-        logger.debug('get_template: page_template: {0}'.format(page_template))
-        default_template = super(BasePage, self).get_template(request,
-                                                              *args, **kwargs)
-        logger.debug('get_template: default_template:{0}'.format(
-            default_template))
+        page_template = "{0}.html".format(self.url.strip("/"))
+        logger.debug("get_template: page_template: {0}".format(page_template))
+        default_template = super(BasePage, self).get_template(request, *args, **kwargs)
+        logger.debug("get_template: default_template:{0}".format(default_template))
 
         template = select_template([page_template, default_template])
 
-        logger.debug('get_template: {0}'.format(template.template.name))
+        logger.debug("get_template: {0}".format(template.template.name))
         return template.template.name
 
 
@@ -136,9 +141,10 @@ class BaseIndexPage(RoutablePageMixin, BasePage):
 
     """Base class for index pages. Index pages are pages that will have
     children pages."""
+
     introduction = RichTextField(blank=True)
 
-    search_fields = Page.search_fields + [index.SearchField('introduction')]
+    search_fields = Page.search_fields + [index.SearchField("introduction")]
 
     is_creatable = False
 
@@ -147,13 +153,13 @@ class BaseIndexPage(RoutablePageMixin, BasePage):
         """Returns a list of the pages that are children of this page."""
         return self.get_children().filter(live=True)
 
-    @route(r'^$')
+    @route(r"^$")
     def serve_listing(self, request):
         """Renders the children pages."""
         pages = self.children
 
         # Pagination
-        page = request.GET.get('page')
+        page = request.GET.get("page")
         paginator = Paginator(pages, settings.ITEMS_PER_PAGE)
 
         try:
@@ -163,16 +169,18 @@ class BaseIndexPage(RoutablePageMixin, BasePage):
         except PageNotAnInteger:
             pages = paginator.page(1)
 
-        return render(request, self.get_template(request),
-                      {'self': self, 'pages': pages})
+        return render(
+            request, self.get_template(request), {"self": self, "pages": pages}
+        )
 
 
 class BaseRichTextPage(BasePage):
 
     """Base class for rich text pages."""
+
     content = RichTextField()
 
-    search_fields = Page.search_fields + [index.SearchField('content')]
+    search_fields = Page.search_fields + [index.SearchField("content")]
 
     is_creatable = False
 
